e7c5763e193c76439ce6201f41bc348157749603
sunwoo <133613205+ssuunnwwoooo@users.noreply.github.com>
2024-06-02 22:43:56 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,11 @@
-$(document).ready(function(){
-    let index = 0; // 현재 슬라이드 인덱스
-
-    const slideWidth = 1200; // 슬라이드 하나의 너비
-
-    const slideCount = $('.slider img').length; // 슬라이드 이미지 개수
-
-    function showNextSlide() {
-        index = (index + 1) % slideCount; // 인덱스를 1 증가, 슬라이드 개수로 나눈 나머지
-        
-        $('.slider').animate({left: -index * slideWidth + 'px'}, 500); // 슬라이드를 이동
+var currentIndex = 0;
+var slidePosition;
+setInterval(function(){
+    if (currentIndex < 2) {
+        currentIndex ++;
+    } else {
+        currentIndex = 0;
     }
-
-    setInterval(showNextSlide, 3000); // 3초마다 showNextSlide 함수 실행
-});
+        slidePosition = currentIndex * (-1200) +"px";
+        $(".slideList").animate({left:slidePosition},400);
+},3000);
