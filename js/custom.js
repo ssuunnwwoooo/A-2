35fbeb11d03e21cc2a3e61ab2abd1f8e9a6e2464
sunwoo <133613205+ssuunnwwoooo@users.noreply.github.com>
2024-06-02 23:01:30 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,20 @@
-var currentIndex = 0;
-var slidePosition;
-setInterval(function(){
-    if (currentIndex < 2) {
-        currentIndex ++;
-    } else {
-        currentIndex = 0;
-    }
-        slidePosition = currentIndex * (-1200) +"px";
-        $(".slideList").animate({left:slidePosition},400);
-},3000);
+$(document).ready(function(){
+    var currentIndex = 0;
+    var slidePosition;
+    setInterval(function(){
+        if (currentIndex < 2) {
+            currentIndex ++;
+        } else {
+            currentIndex = 0;
+        }
+            slidePosition = currentIndex * (-1200) +"px";
+            $(".slideList").animate({left:slidePosition},400);
+    },3000);
+
+    $('.open-modal').click(function(){
+        $('.modal').fadeIn()
+    })
+    $('.modal-close').click(function(){
+        $('.modal').fadeOut()
+    })
+});
